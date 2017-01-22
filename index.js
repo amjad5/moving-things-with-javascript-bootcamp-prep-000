0d81a21a4c44c3f1152e9ada125dad756bfd47e4
Amjad <amjad.hussain544@gmail.com>
2017-01-22 23:50:14 +0500

    Done!
    Dodger moving right and left

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-const app = "I don't do much."
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Rock Dodger! (Beta)</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+  </head>
+
+  <body>
+    <div id="game">
+      <div id="dodger" style="bottom: 0px; left: 180px; right:180px;"></div>
+    </div>
+    <script>
+    var dodger = document.getElementById('dodger');
+	  //dodger.style.backgroundColor = "#000000";
+	  dodger.style.backgroundColor = '#FF69B4';
+    
+    document.addEventListener('keydown', function(e) {
+      if (e.which === 37) {
+        moveDodgerLeft();
+      }else if(e.which === 39){
+        moveDodgerRight();
+      }  
+    });
+
+    function moveDodgerLeft() {
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
+      var rightNumbers = dodger.style.right.replace('px', '')
+      var right = parseInt(rightNumbers, 10)
+
+      if (left > 0) {
+        dodger.style.left = `${left - 4}px`
+        dodger.style.right = `${right + 4}px`
+        }
+      }
+
+    function moveDodgerRight() {
+      var rightNumbers = dodger.style.right.replace('px', '')
+      var right = parseInt(rightNumbers, 10)
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
+
+      if (right > 0) {
+        dodger.style.right = `${right - 4}px`
+        dodger.style.left = `${left + 4}px`
+        }
+      }
+    </script>
+  </body>
+</html>
